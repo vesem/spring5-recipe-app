de7493acec2fb39e65a99d07c7081145198e7d0f
vesem <mitchellthom@gmail.com>
2020-07-14 22:27:51 -0400

    Resolved failing tests

--- src/main/java/guru/springframework/converters/IngredientToIngredientCommand.java
+++ src/main/java/guru/springframework/converters/IngredientToIngredientCommand.java
@@ -27,6 +27,9 @@ public class IngredientToIngredientCommand implements Converter<Ingredient, Ingr
         IngredientCommand ingredientCommand = new IngredientCommand();
         ingredientCommand.setId(ingredient.getId());
         ingredientCommand.setAmount(ingredient.getAmount());
+        if (ingredient.getRecipe() != null) {
+            ingredientCommand.setRecipeId(ingredient.getRecipe().getId());
+        }
         ingredientCommand.setDescription(ingredient.getDescription());
         ingredientCommand.setUom(uomConverter.convert(ingredient.getUom()));
         return ingredientCommand;
